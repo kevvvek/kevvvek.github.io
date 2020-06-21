41479ed43dbf41d94e60e8550715de9d1b8219b9
kevvvek <github.kevinchow@gmail.com>
2020-06-22 00:59:07 +0800

    day5

---
+++ precache-manifest.6aeb64eccb7c055d80d274e94f0de7b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad786a12b3554a12dfe092bd03c55c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f78515127d0cba6cde5a",
+    "url": "/static/css/main.c4ce8a09.chunk.css"
+  },
+  {
+    "revision": "9dd69db14a6f8a649b9f",
+    "url": "/static/js/2.ffbf79e4.chunk.js"
+  },
+  {
+    "revision": "19436aa5872d75cdbbc02e6c3c56f9c3",
+    "url": "/static/js/2.ffbf79e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78515127d0cba6cde5a",
+    "url": "/static/js/main.65fc8c2f.chunk.js"
+  },
+  {
+    "revision": "7f0467aafa5812ee99dc",
+    "url": "/static/js/runtime-main.ee8a8e31.js"
+  }
+]);
